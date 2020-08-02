c837f8560d10bffd84788b97efb60dcbed80fd0d
devWRM <mcwen@comcast.net>
2020-08-02 16:55:01 -0400

    Added the get '/ideas' route-method to check that the logged in email entered is the session[:email]

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,11 @@
 
 class IdeasController < ApplicationController
 
-
+    get '/ideas' do
+        # binding.pry       <= session
+        # raise session[:email].inspect
+        "You're currently logged in as " + session[:email]
+    end
 
 
 end
