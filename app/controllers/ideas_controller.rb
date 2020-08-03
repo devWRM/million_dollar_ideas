daace4ad54376b141c67fb1b01bf8e2901afdc57
devWRM <mcwen@comcast.net>
2020-08-03 07:40:48 -0400

    Set up the new and edit block methods inside of the IdeasController, also staged each with an if statement using the logged_in? helper method

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,11 +2,38 @@
 class IdeasController < ApplicationController
 
     get '/ideas' do
-binding.pry
+
         # binding.pry       <= session
         # raise session[:email].inspect
         "You're currently logged in as " + session[:email]
     end
+
+    get '/ideas/new' do
+
+        if !logged_in?
+            redirect "/login"
+        else
+            "Build a new idea form"
+        end
+
+    end
+
+    get '/ideas/:id/edit' do
+
+        if !logged_in?
+            redirect "/login"
+        else
+            "Build an edit idea form"
+        end
+
+    end
+
+
+
+
+
+
+
 
 
 end
