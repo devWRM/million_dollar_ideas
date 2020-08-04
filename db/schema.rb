185905e16b6b224d4ccd14992d254c8d5789cb73
devWRM <mcwen@comcast.net>
2020-08-04 14:10:33 -0400

    Added a user_id column to the ideas table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_224941) do
+ActiveRecord::Schema.define(version: 2020_08_04_032052) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_224941) do
     t.integer "total_spent_dollars"
     t.integer "remaining_budgeted_dollars"
     t.boolean "within_budget"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
