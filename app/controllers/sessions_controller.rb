1be8203f3dcbe740063c797a96fd6c59f3ffc95d
devWRM <mcwen@comcast.net>
2020-08-03 23:03:56 -0400

    Re-working the code for login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,25 @@ class SessionsController < ApplicationController
         login(params[:email])                # Transferred to login helpers: session[:email] = params[:email]
         # raise session[:email].inspect
 
+
+
+
+        # 26:30 Howard video:  https://www.youtube.com/watch?v=U5glnUNaJPs&feature=youtu.be
+
+        # Find/match user with email key and value params[:email]
+        @user = User.find_by(email: params[:email])
+
+        # Authenticate user with email & password
+        @user.authenticate(params[:password])
+
+        # NOTE 30:50 tux Howard video:  https://www.youtube.com/watch?v=U5glnUNaJPs&feature=youtu.be
+        # 20:00 tux & 52:00 seed  Howard video:   https://www.youtube.com/watch?v=y5XHoP5qwfY&feature=youtu.be
+        
+        # Log in the user
+        # redirect to user's landing page
+
+
+
         redirect '/ideas'
     end
 
