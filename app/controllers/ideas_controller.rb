233dc6a4835c364341196872b1e3f66d31f54482
devWRM <mcwen@comcast.net>
2020-08-13 21:05:47 -0400

    Refactored patch to delete _method from params so can now mass assign params, also added if statement to ensure 4 mandatories must have data. Refactored get '/ideas/:id' to use .find_by to use nil in condition in the added if statement

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -16,34 +16,7 @@ class IdeasController < ApplicationController
         erb :'ideas/index'
     end
 
-    get '/test' do
-
-        # binding.pry       <= session hash exists!
-        # raise session[:email].inspect
-
-        ## logged_in? is working BUT absent emails still considered logged in SOLUTION: Had to refer directly to "" in session[:email] so now the if statement works BUT without the "Login please" message
-        ### NOTE session[:email] == string (empty or not)
-        ### NOTE session == hash (that contains an email key with a string value that's empty or not)
-
-        ### NOTE !session[:email] & !logged_in? have the same false value (when email address NOT entered) so why do I need to include both in the if statement ???
-        # Are both !session[:email] and !logged_in? testing for the same thing ???
-        # NO!!!! They are not! Because: 
-        # session[:email] tests if the email key exists or not WHILE... 
-        # logged_in? tests for an empty string value
-
-        # logged_in refactored to consider both if email key exists && if the string value is not empty
-                if !logged_in?
-                    "Login please"
-                    redirect "/login"
-                end
-
-        # "You've reached the IDEAS page."
-        ##### Why did the below stop working:
-        # "You're currently logged in as " + session[:email]
-        # "You're currently logged in as #{session[:email]}"
-        "Welcome to IDEAS, user #{User.find_by(email: session[:email]).name}!"
-        # User.find_by(email: session[:email]).name
-    end
+    
 
     get '/ideas/new' do
         # Render a form to create a new idea
@@ -102,13 +75,17 @@ class IdeasController < ApplicationController
     end
 
     get '/ideas/:id' do
-        # Dynamic route because it changes to find a specific idea
-    # binding.pry
-
+        # Dynamic route because it changes to find a specific idea  
+# binding.pry
         # set_idea replaces: @idea = Idea.find(params[:id])
         # set_idea
-        @idea = Idea.find(params[:id])
+        # @idea = Idea.find(params[:id]) 
+        @idea = Idea.find_by(id: params[:id])
+        if @idea == nil
+            redirect '/ideas'
+        end
 
+# binding.pry
         erb :'/ideas/show'
 
     end
@@ -160,14 +137,22 @@ class IdeasController < ApplicationController
         # Find the idea:    Idea.find(params[:id])
         # set_idea finds the idea, replaces:    @idea = Idea.find(params[:id])
         set_idea
+        # ...if any of the 4 params has no data 
+        if params[:title] == "" && params[:category] != "" && params[:inspiration] != "" && params[:summary] != ""
+            redirect "/ideas"
+        end
+
 
         if logged_in?
                 if @idea.user == current_user
 
+                    params.delete("_method")
+                    @idea.update(params)
+
                     # Update the selected idea
                     # CANNOT !!! mass assign because patch method in hash unknown to ActiveRecord:  @idea.update(params)
-                    @idea.update({title: params[:title], category: params[:category], inspiration: params[:inspiration], summary: params[:summary], timeline_plan: params[:timeline_plan], action_steps_accomplished: params[:action_steps_accomplished], resources: params[:resources], total_budgeted_dollars: params[:total_budgeted_dollars], total_spent_dollars: params[:total_spent_dollars]})
-                        
+                        # @idea.update({title: params[:title], category: params[:category], inspiration: params[:inspiration], summary: params[:summary], timeline_plan: params[:timeline_plan], action_steps_accomplished: params[:action_steps_accomplished], resources: params[:resources], total_budgeted_dollars: params[:total_budgeted_dollars], total_spent_dollars: params[:total_spent_dollars]})
+                            
                     # Redirect to the show page to see the updated idea
                     # ORIG: redirect "/ideas/#{@idea[:id]}"
                     redirect "/ideas/#{@idea.id}"
